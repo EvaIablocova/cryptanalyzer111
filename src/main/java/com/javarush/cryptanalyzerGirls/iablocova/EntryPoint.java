3efc84b68d3628aa81dfe00d3eddd13bfde1f66f
Eva <evaiablocova@gmail.com>
2024-01-25 08:35:41 +0200

    version 14. StatisticalAnalysis - changeCharacters

--- src/main/java/com/javarush/cryptanalyzerGirls/iablocova/EntryPoint.java
+++ src/main/java/com/javarush/cryptanalyzerGirls/iablocova/EntryPoint.java
@@ -3,7 +3,7 @@ package com.javarush.cryptanalyzerGirls.iablocova;
 import com.javarush.cryptanalyzerGirls.iablocova.app.Application;
 import com.javarush.cryptanalyzerGirls.iablocova.controller.MainController;
 import com.javarush.cryptanalyzerGirls.iablocova.entity.Result;
-//import com.javarush.cryptanalyzerGirls.iablocova.view.ConsoleView;
+import com.javarush.cryptanalyzerGirls.iablocova.view.ConsoleView;
 import com.javarush.cryptanalyzerGirls.iablocova.view.GUIView;
 import com.javarush.cryptanalyzerGirls.iablocova.view.View;
 
@@ -11,7 +11,7 @@ import com.javarush.cryptanalyzerGirls.iablocova.view.View;
 public class EntryPoint {
     public static void main (String[] args){
             //спрашивать в начале какой view -> создаем view
-            View view = new GUIView();
+            View view = new ConsoleView();
             MainController mainController = new MainController(view);
             Application application = new Application(mainController);
 
--- src/main/java/com/javarush/cryptanalyzerGirls/iablocova/services/StatisticalAnalysis.java
+++ src/main/java/com/javarush/cryptanalyzerGirls/iablocova/services/StatisticalAnalysis.java
@@ -30,53 +30,16 @@ public class StatisticalAnalysis implements Function {
 
             convertFileToLowerCase (fileDictionary, fileDictionary);
 
-
             Map<Character, Integer> encodedStatistics = countCharactersInEncodedFile(encodedFile);
-
-//            File dictionary = new File(parameters[2]);
-//            if (!dictionary.exists()) {
-//                dictionary.createNewFile();
-//            }
 
             Map<Character, Integer> dictionaryStatistics = countCharactersInDictionaryFile(encodedStatistics, fileDictionary);
 
             makeMapsEqual(dictionaryStatistics, encodedStatistics);
 
             replaceCharactersFromStatistics(encodedFile, decodedFile, encodedStatistics, dictionaryStatistics);
-            System.out.println("Enter the characters you want to swap or enter 'exit' to quit:");
-            Scanner scanner = new Scanner(System.in);
 
-            while (true) {
-                System.out.print("Source Character: ");
-                String sourceInput = scanner.nextLine();
+            changeCharacters (decodedFile);
 
-                if (sourceInput.equalsIgnoreCase("exit")) {
-                    break;
-                }
-
-                if (sourceInput.length() != 1) {
-                    System.out.println("Invalid input. Please enter a single character.");
-                    continue;
-                }
-
-                char sourceChar = sourceInput.charAt(0);
-
-                System.out.print("Target Character: ");
-                String targetInput = scanner.nextLine();
-
-                if (targetInput.equalsIgnoreCase("exit")) {
-                    break;
-                }
-                if (targetInput.length() != 1) {
-                    System.out.println("Invalid input. Please enter a single character.");
-                    continue;
-                }
-
-                char targetChar = targetInput.charAt(0);
-
-                swapCharactersInFile(decodedFile, sourceChar, targetChar);
-                System.out.println("Characters swapped successfully.");
-            }
         }  catch (IOException e) {
             e.printStackTrace();
         }
@@ -298,6 +261,43 @@ public class StatisticalAnalysis implements Function {
             rewriteTextToFile(file, newContent.toString());
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void changeCharacters (File decodedFile) {
+        System.out.println("Enter the characters you want to swap or enter 'exit' to quit:");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Source Character: ");
+            String sourceInput = scanner.nextLine();
+
+            if (sourceInput.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            if (sourceInput.length() != 1) {
+                System.out.println("Invalid input. Please enter a single character.");
+                continue;
+            }
+
+            char sourceChar = sourceInput.charAt(0);
+
+            System.out.print("Target Character: ");
+            String targetInput = scanner.nextLine();
+
+            if (targetInput.equalsIgnoreCase("exit")) {
+                break;
+            }
+            if (targetInput.length() != 1) {
+                System.out.println("Invalid input. Please enter a single character.");
+                continue;
+            }
+
+            char targetChar = targetInput.charAt(0);
+
+            swapCharactersInFile(decodedFile, sourceChar, targetChar);
+            System.out.println("Characters swapped successfully.");
         }
     }
 }
